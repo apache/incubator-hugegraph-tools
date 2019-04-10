miofy/hugegraph-tools

Improve rebust for backup huge data size
1. compress vertices and edges backup data
2. process 80w limit exceed exception
3. set timeout argument
4. add HDFS support

Change-Id: I2976270f8d8e6970f5966f48a5d91e9ab7056ce1

src/main/java/com/baidu/hugegraph/manager/BackupRestoreBaseManager.java
@@ -1,0 +1,251 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.manager;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.function.BiConsumer;
+
+import com.baidu.hugegraph.api.API;
+import com.baidu.hugegraph.base.DirectoryOperator;
+import com.baidu.hugegraph.base.HdfsDirectoryOperator;
+import com.baidu.hugegraph.base.LocalDirectoryOperator;
+import com.baidu.hugegraph.base.Printer;
+import com.baidu.hugegraph.base.RetryManager;
+import com.baidu.hugegraph.base.ToolClient;
+import com.baidu.hugegraph.cmd.SubCommands;
+import com.baidu.hugegraph.concurrent.KeyLock;
+import com.baidu.hugegraph.exception.ToolsException;
+import com.baidu.hugegraph.structure.constant.HugeType;
+import com.baidu.hugegraph.util.E;
+import com.google.common.collect.ImmutableMap;
+
+public class BackupRestoreBaseManager extends RetryManager {
+
+    public static final String HDFS_PREFIX = "hdfs://";
+    protected static final int LBUF_SIZE = 1024;
+
+    protected AtomicLong propertyKeyCounter = new AtomicLong(0);
+    protected AtomicLong vertexLabelCounter = new AtomicLong(0);
+    protected AtomicLong edgeLabelCounter = new AtomicLong(0);
+    protected AtomicLong indexLabelCounter = new AtomicLong(0);
+    protected AtomicLong vertexCounter = new AtomicLong(0);
+    protected AtomicLong edgeCounter = new AtomicLong(0);
+
+    private long startTime = 0L;
+    protected static KeyLock locks = new KeyLock();
+    private String logDir;
+    private Map<String, String> hdfsConf = new HashMap<>();
+    private DirectoryOperator operator;
+    private Map<String, OutputStream> outputStreams;
+    private Map<String, InputStream> inputStreams;
+
+    public BackupRestoreBaseManager(ToolClient.ConnectionInfo info,
+                                    String type) {
+        super(info, type);
+        this.outputStreams = new ConcurrentHashMap<>();
+        this.inputStreams = new ConcurrentHashMap<>();
+    }
+
+    public void init(SubCommands.BackupRestore cmd) {
+        assert cmd.retry() > 0;
+        this.retry(cmd.retry());
+        ensureDirectoryExist(cmd.logDir());
+        this.logDir(cmd.logDir());
+        String directory = cmd.directory();
+        E.checkArgument(directory != null && !directory.isEmpty(),
+                        "Directory of backup/restore can't be null or empty");
+        this.hdfsConf = cmd.hdfsConf();
+        if (hdfsDir(directory)) {
+            this.operator = new HdfsDirectoryOperator(directory, this.hdfsConf);
+        } else {
+            this.operator = new LocalDirectoryOperator(directory);
+        }
+    }
+
+    public void logDir(String logDir) {
+        this.logDir = logDir;
+    }
+
+    public String logDir() {
+        return this.logDir;
+    }
+
+    protected void ensureDirectoryExist(boolean create) {
+        this.operator.ensureDirectoryExist(create);
+    }
+
+    protected void write(String path, HugeType type, List<?> list) {
+        OutputStream os = this.outputStream(path);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(LBUF_SIZE);
+        try {
+            String key = String.format("{\"%s\": ", type.string());
+            baos.write(key.getBytes(API.CHARSET));
+            this.client.mapper().writeValue(baos, list);
+            baos.write("}\n".getBytes(API.CHARSET));
+            os.write(baos.toByteArray());
+        } catch (Exception e) {
+            throw new ToolsException("Failed to serialize %s to %s",
+                                     e, type, path);
+        }
+    }
+
+    protected void read(String file, HugeType type,
+                        BiConsumer<String, String> consumer) {
+        InputStream is = this.inputStream(file);
+        try (InputStreamReader isr = new InputStreamReader(is, API.CHARSET);
+             BufferedReader reader = new BufferedReader(isr)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                consumer.accept(type.string(), line);
+            }
+        } catch (IOException e) {
+            throw new ToolsException("Failed to deserialize %s from %s",
+                                     e, type, file);
+        }
+    }
+
+    private OutputStream outputStream(String file) {
+        OutputStream os;
+        if (this.outputStreams.get(file) == null) {
+            os = this.operator.outputStream(file, true);
+            this.outputStreams.putIfAbsent(file, os);
+        }
+        return this.outputStreams.get(file);
+    }
+
+    private InputStream inputStream(String file) {
+        InputStream is;
+        if (this.inputStreams.get(file) == null) {
+            is = this.operator.inputStream(file);
+            this.inputStreams.putIfAbsent(file, is);
+        }
+        return this.inputStreams.get(file);
+    }
+
+    protected List<String> filesWithPrefix(HugeType type) {
+        List<String> files = new ArrayList<>();
+        for (String file : this.operator.files()) {
+            if (file.startsWith(type.string())) {
+                files.add(file);
+            }
+        }
+        return files;
+    }
+
+    protected String suffix() {
+        return this.operator.suffix();
+    }
+
+    public void startTimer() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long elapseSeconds() {
+        E.checkState(this.startTime != 0,
+                     "Must call startTimer() to set start time, " +
+                     "before call elapse()");
+        return (System.currentTimeMillis() - this.startTime) / 1000;
+    }
+
+    protected void printSummary() {
+        this.printSummary(this.type());
+    }
+
+    protected void printSummary(String type) {
+        Map<String, Long> summary = ImmutableMap.<String, Long>builder()
+                .put("property key number", this.propertyKeyCounter.longValue())
+                .put("vertex label number", this.vertexLabelCounter.longValue())
+                .put("edge label number", this.edgeLabelCounter.longValue())
+                .put("index label number", this.indexLabelCounter.longValue())
+                .put("vertex number", this.vertexCounter.longValue())
+                .put("edge number", this.edgeCounter.longValue()).build();
+        Printer.printMap(type + " summary", summary);
+
+        Printer.printKV("cost time(s)", this.elapseSeconds());
+    }
+
+    protected static void ensureDirectoryExist(String dir) {
+        File file = new File(dir);
+        if (file.exists()) {
+            E.checkState(file.isDirectory(),
+                         "Can't use directory '%s' because a file with " +
+                         "same name exists.", file.getAbsolutePath());
+        } else {
+            E.checkState(file.mkdirs(),
+                         "Directory '%s' not exists and created failed", dir);
+        }
+    }
+
+    private static boolean hdfsDir(String directory) {
+        return directory.startsWith(HDFS_PREFIX);
+    }
+
+    protected void write(String file, String type, List<?> list) {
+        Lock lock = locks.lock(file);
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(LBUF_SIZE);
+             FileOutputStream fos = new FileOutputStream(file, false)) {
+            String key = String.format("{\"%s\": ", type);
+            baos.write(key.getBytes(API.CHARSET));
+            this.client.mapper().writeValue(baos, list);
+            baos.write("}\n".getBytes(API.CHARSET));
+
+            fos.write(baos.toByteArray());
+        } catch (Exception e) {
+            Printer.print("Failed to serialize %s: %s", type, e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void shutdown(String taskType) {
+        super.shutdown(taskType);
+        for (Map.Entry<String, OutputStream> e : this.outputStreams.entrySet()) {
+            try {
+                OutputStream os = e.getValue();
+                os.close();
+            } catch (IOException exception) {
+                Printer.print("Failed to close file '%s'", e.getKey());
+            }
+        }
+        for (Map.Entry<String, InputStream> e : this.inputStreams.entrySet()) {
+            try {
+                InputStream is = e.getValue();
+                is.close();
+            } catch (IOException exception) {
+                Printer.print("Failed to close file '%s'", e.getKey());
+            }
+        }
+    }
+}

apache/incubator-hugegraph-tools

src/main/java/com/baidu/hugegraph/manager/BackupRestoreBaseManager.java
@@ -1,0 +1,243 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.manager;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.function.BiConsumer;
+
+import com.baidu.hugegraph.api.API;
+import com.baidu.hugegraph.base.Directory;
+import com.baidu.hugegraph.base.HdfsDirectory;
+import com.baidu.hugegraph.base.LocalDirectory;
+import com.baidu.hugegraph.base.Printer;
+import com.baidu.hugegraph.base.RetryManager;
+import com.baidu.hugegraph.base.ToolClient;
+import com.baidu.hugegraph.cmd.SubCommands;
+import com.baidu.hugegraph.concurrent.KeyLock;
+import com.baidu.hugegraph.exception.ToolsException;
+import com.baidu.hugegraph.structure.constant.HugeType;
+import com.baidu.hugegraph.util.E;
+import com.google.common.collect.ImmutableMap;
+
+public class BackupRestoreBaseManager extends RetryManager {
+
+    public static final String HDFS_PREFIX = "hdfs://";
+    protected static final int LBUF_SIZE = 1024;
+
+    protected AtomicLong propertyKeyCounter = new AtomicLong(0);
+    protected AtomicLong vertexLabelCounter = new AtomicLong(0);
+    protected AtomicLong edgeLabelCounter = new AtomicLong(0);
+    protected AtomicLong indexLabelCounter = new AtomicLong(0);
+    protected AtomicLong vertexCounter = new AtomicLong(0);
+    protected AtomicLong edgeCounter = new AtomicLong(0);
+
+    private long startTime = 0L;
+    protected static KeyLock locks = new KeyLock();
+    private String logDir;
+    private Map<String, String> hdfsConf = new HashMap<>();
+    protected Directory directory;
+    private Map<String, OutputStream> outputStreams;
+    private Map<String, InputStream> inputStreams;
+
+    public BackupRestoreBaseManager(ToolClient.ConnectionInfo info,
+                                    String type) {
+        super(info, type);
+        this.outputStreams = new ConcurrentHashMap<>();
+        this.inputStreams = new ConcurrentHashMap<>();
+    }
+
+    public void init(SubCommands.BackupRestore cmd) {
+        assert cmd.retry() > 0;
+        this.retry(cmd.retry());
+        ensureDirectoryExist(cmd.logDir());
+        this.logDir(cmd.logDir());
+        String directory = cmd.directory();
+        E.checkArgument(directory != null && !directory.isEmpty(),
+                        "Directory of backup/restore can't be null or empty");
+        this.hdfsConf = cmd.hdfsConf();
+        if (directory.startsWith(HDFS_PREFIX)) {
+            this.directory = new HdfsDirectory(directory, this.hdfsConf);
+        } else {
+            this.directory = new LocalDirectory(directory);
+        }
+    }
+
+    public void logDir(String logDir) {
+        this.logDir = logDir;
+    }
+
+    public String logDir() {
+        return this.logDir;
+    }
+
+    protected void ensureDirectoryExist(boolean create) {
+        this.directory.ensureDirectoryExist(create);
+    }
+
+    protected void write(String path, HugeType type, List<?> list) {
+        OutputStream os = this.outputStream(path);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(LBUF_SIZE);
+        try {
+            String key = String.format("{\"%s\": ", type.string());
+            baos.write(key.getBytes(API.CHARSET));
+            this.client.mapper().writeValue(baos, list);
+            baos.write("}\n".getBytes(API.CHARSET));
+            os.write(baos.toByteArray());
+        } catch (Exception e) {
+            throw new ToolsException("Failed to serialize %s to %s",
+                                     e, type, path);
+        }
+    }
+
+    protected void read(String file, HugeType type,
+                        BiConsumer<String, String> consumer) {
+        InputStream is = this.inputStream(file);
+        try (InputStreamReader isr = new InputStreamReader(is, API.CHARSET);
+             BufferedReader reader = new BufferedReader(isr)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                consumer.accept(type.string(), line);
+            }
+        } catch (IOException e) {
+            throw new ToolsException("Failed to deserialize %s from %s",
+                                     e, type, file);
+        }
+    }
+
+    private OutputStream outputStream(String file) {
+        OutputStream os;
+        if (this.outputStreams.get(file) == null) {
+            os = this.directory.outputStream(file, true);
+            this.outputStreams.putIfAbsent(file, os);
+        }
+        return this.outputStreams.get(file);
+    }
+
+    private InputStream inputStream(String file) {
+        InputStream is;
+        if (this.inputStreams.get(file) == null) {
+            is = this.directory.inputStream(file);
+            this.inputStreams.putIfAbsent(file, is);
+        }
+        return this.inputStreams.get(file);
+    }
+
+    protected List<String> filesWithPrefix(HugeType type) {
+        List<String> files = new ArrayList<>();
+        for (String file : this.directory.files()) {
+            if (file.startsWith(type.string())) {
+                files.add(file);
+            }
+        }
+        return files;
+    }
+
+    public void startTimer() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long elapseSeconds() {
+        E.checkState(this.startTime != 0,
+                     "Must call startTimer() to set start time, " +
+                     "before call elapse()");
+        return (System.currentTimeMillis() - this.startTime) / 1000;
+    }
+
+    protected void printSummary() {
+        this.printSummary(this.type());
+    }
+
+    protected void printSummary(String type) {
+        Map<String, Long> summary = ImmutableMap.<String, Long>builder()
+                .put("property key number", this.propertyKeyCounter.longValue())
+                .put("vertex label number", this.vertexLabelCounter.longValue())
+                .put("edge label number", this.edgeLabelCounter.longValue())
+                .put("index label number", this.indexLabelCounter.longValue())
+                .put("vertex number", this.vertexCounter.longValue())
+                .put("edge number", this.edgeCounter.longValue()).build();
+        Printer.printMap(type + " summary", summary);
+
+        Printer.printKV("cost time(s)", this.elapseSeconds());
+    }
+
+    protected static void ensureDirectoryExist(String dir) {
+        File file = new File(dir);
+        if (file.exists()) {
+            E.checkState(file.isDirectory(),
+                         "Can't use directory '%s' because a file with " +
+                         "same name exists.", file.getAbsolutePath());
+        } else {
+            E.checkState(file.mkdirs(),
+                         "Directory '%s' not exists and created failed", dir);
+        }
+    }
+
+    protected void write(String file, String type, List<?> list) {
+        Lock lock = locks.lock(file);
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(LBUF_SIZE);
+             FileOutputStream fos = new FileOutputStream(file, false)) {
+            String key = String.format("{\"%s\": ", type);
+            baos.write(key.getBytes(API.CHARSET));
+            this.client.mapper().writeValue(baos, list);
+            baos.write("}\n".getBytes(API.CHARSET));
+
+            fos.write(baos.toByteArray());
+        } catch (Exception e) {
+            Printer.print("Failed to serialize %s: %s", type, e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void shutdown(String taskType) {
+        super.shutdown(taskType);
+        for (Map.Entry<String, OutputStream> e : this.outputStreams.entrySet()) {
+            try {
+                OutputStream os = e.getValue();
+                os.close();
+            } catch (IOException exception) {
+                Printer.print("Failed to close file '%s'", e.getKey());
+            }
+        }
+        for (Map.Entry<String, InputStream> e : this.inputStreams.entrySet()) {
+            try {
+                InputStream is = e.getValue();
+                is.close();
+            } catch (IOException exception) {
+                Printer.print("Failed to close file '%s'", e.getKey());
+            }
+        }
+    }
+}