26f783a06bbbf6a4f9ce1fa5c87c8df493e420b2
zhangyi51 <zhangyi51@baidu.com>
2021-12-27 20:54:49 +0800

    upgrade version to 1.6.0
    Change-Id: Ic81ff6617ff962b015c7b25149a5c05dc878939e

--- assembly/travis/install-hugegraph.sh
+++ assembly/travis/install-hugegraph.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
-
 set -ev
-
-TRAVIS_DIR=`dirname $0`
-
-if [ $# -ne 1 ]; then
-    echo "Must pass base branch name of pull request"
+if [[ $# -ne 1 ]]; then
+    echo "Must pass commit id of hugegraph repo"
     exit 1
 fi
 
-CLIENT_BRANCH=$1
-HUGEGRAPH_BRANCH=$CLIENT_BRANCH
-
+COMMIT_ID=$1
 HUGEGRAPH_GIT_URL="https://github.com/hugegraph/hugegraph.git"
+GIT_DIR=hugegraph
 
-git clone $HUGEGRAPH_GIT_URL
-
-cd hugegraph
-
-git checkout $HUGEGRAPH_BRANCH
-
+# download code and compile
+git clone --depth 100 ${HUGEGRAPH_GIT_URL}
+cd "${GIT_DIR}"
+git checkout ${COMMIT_ID}
 mvn package -DskipTests
 
-mv hugegraph-*.tar.gz ../
-
+TAR=$(echo hugegraph-*.tar.gz)
+tar -zxvf "${TAR}" -C ../
 cd ../
+rm -rf "${GIT_DIR}"
 
-rm -rf hugegraph
-
-tar -zxvf hugegraph-*.tar.gz
-
+HTTP_SERVER_DIR=$(echo hugegraph-*)
 HTTPS_SERVER_DIR="hugegraph_https"
 
-mkdir $HTTPS_SERVER_DIR
+cp -r "${HTTP_SERVER_DIR}" "${HTTPS_SERVER_DIR}"
 
-cp -r hugegraph-*/. $HTTPS_SERVER_DIR
+# config auth options just for http server (must keep '/.')
+cp -rf "${TRAVIS_DIR}"/conf/. "${HTTP_SERVER_DIR}"/conf/
 
-cd hugegraph-*
+# start HugeGraphServer with http protocol
+cd "${HTTP_SERVER_DIR}"
+echo -e "pa" | bin/init-store.sh || exit 1
+bin/start-hugegraph.sh || exit 1
 
-cp ../$TRAVIS_DIR/conf/* conf
-
-echo -e "123456" | bin/init-store.sh
-
+# config options for https server
+cd ../"${HTTPS_SERVER_DIR}"
+REST_SERVER_CONFIG="conf/rest-server.properties"
+GREMLIN_SERVER_CONFIG="conf/gremlin-server.yaml"
+sed -i "s?http://127.0.0.1:8080?https://127.0.0.1:8443?g" "$REST_SERVER_CONFIG"
+sed -i "s/#port: 8182/port: 8282/g" "$GREMLIN_SERVER_CONFIG"
+echo "gremlinserver.url=http://127.0.0.1:8282" >> ${REST_SERVER_CONFIG}
+# start HugeGraphServer with https protocol
+bin/init-store.sh
 bin/start-hugegraph.sh
+cd ../
