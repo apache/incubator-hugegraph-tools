starhugegraph/hugegraph-tools

support graph-create and graph-drop subcommand (#2)
Change-Id: Ifcf56e1002c3c93ddc9401ae3631d6102ac17fea

src/main/java/com/baidu/hugegraph/manager/GraphsManager.java
@@ -32,6 +32,10 @@ public class GraphsManager extends ToolManager {
         super(info, "graphs");
     }
 
+    public Map<String, String> create(String name, String config) {
+        return this.client.graphs().createGraph(name, config);
+    }
+
     public List<String> list() {
         return this.client.graphs().listGraph();
     }
@@ -42,6 +46,10 @@ public class GraphsManager extends ToolManager {
 
     public void clear(String graph, String confirmMessage) {
         this.client.graphs().clear(graph, confirmMessage);
+    }
+
+    public void drop(String graph, String confirmMessage) {
+        this.client.graphs().remove(graph, confirmMessage);
     }
 
     public void mode(String graph, GraphMode mode) {

apache/incubator-hugegraph-tools

src/main/java/com/baidu/hugegraph/manager/GraphsManager.java
@@ -32,6 +32,10 @@ public class GraphsManager extends ToolManager {
         super(info, "graphs");
     }
 
+    public Map<String, String> create(String name, String config) {
+        return this.client.graphs().createGraph(name, config);
+    }
+
     public List<String> list() {
         return this.client.graphs().listGraph();
     }
@@ -42,6 +46,10 @@ public class GraphsManager extends ToolManager {
 
     public void clear(String graph, String confirmMessage) {
         this.client.graphs().clearGraph(graph, confirmMessage);
+    }
+
+    public void drop(String graph, String confirmMessage) {
+        this.client.graphs().dropGraph(graph, confirmMessage);
     }
 
     public void mode(String graph, GraphMode mode) {